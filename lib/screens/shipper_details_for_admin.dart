f8774f8b290e1c02a014de5ead614a31f73da819
Ahmet Bugra <bugra.ahmetozcan@gmail.com>
2021-05-07 14:59:49 +0300

    07 05 2021 yeni alanlar eklendi.

--- lib/screens/shipper_details_for_admin.dart
+++ lib/screens/shipper_details_for_admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_biasansor/core/extensions/context_extension.dart';
 import 'package:flutter_biasansor/model/shipper.dart';
 import 'package:flutter_biasansor/viewmodel/viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -12,7 +13,7 @@ class ShipperDetailsForAdmin extends StatefulWidget {
 
 class _ShipperDetailsForAdminState extends State<ShipperDetailsForAdmin> {
   @override
-  var shipper;
+  Shipper shipper;
   Widget build(BuildContext context) {
     var _viewModel = Provider.of<ViewModel>(context);
     return Scaffold(
@@ -26,9 +27,95 @@ class _ShipperDetailsForAdminState extends State<ShipperDetailsForAdmin> {
             shipper = snapshot.data;
             return ListView(
               children: [
-                Text(shipper.phoneNumber),
-                Text(shipper.name ?? ' null'),
-                Text(shipper.fullName ?? ' null'),
+                ListTile(
+                  title: Text("Asansör Şirketinin Adı",
+                      style: context.theme.textTheme.headline6
+                          .copyWith(color: Colors.blue[900])),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.name,
+                  ),
+                ),
+                ListTile(
+                  title: Text("Asansörcünün Tam Adı",
+                      style: context.theme.textTheme.headline6
+                          .copyWith(color: Colors.blue[900])),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.fullName,
+                  ),
+                ),
+                ListTile(
+                  title: Text("Telefon Numarası",
+                      style: context.theme.textTheme.headline6
+                          .copyWith(color: Colors.blue[900])),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.phoneNumber,
+                  ),
+                ),
+                ListTile(
+                  title: Text("Çalıştığı Yerler",
+                      style: context.theme.textTheme.headline6
+                          .copyWith(color: Colors.blue[900])),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.locations.toString(),
+                  ),
+                ),
+                ListTile(
+                  title: Text("Asansörün Ücreti",
+                      style: context.theme.textTheme.headline6
+                          .copyWith(color: Colors.blue[900])),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.shippingPrice.toString(),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Asansörcünün IDsi",
+                    style: context.theme.textTheme.headline6
+                        .copyWith(color: Colors.blue[900]),
+                  ),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.id,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "İkinci Taşıma İndirimi",
+                    style: context.theme.textTheme.headline6
+                        .copyWith(color: Colors.blue[900]),
+                  ),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.secondShippingDiscount.toString(),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "İstisna Katlar",
+                    style: context.theme.textTheme.headline6
+                        .copyWith(color: Colors.blue[900]),
+                  ),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.exceptionFloors.toString(),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Zam Miktarları",
+                    style: context.theme.textTheme.headline6
+                        .copyWith(color: Colors.blue[900]),
+                  ),
+                  subtitle: TextFormField(
+                    readOnly: true,
+                    initialValue: shipper.raiseAmounts.toString(),
+                  ),
+                ),
               ],
             );
           } else {
